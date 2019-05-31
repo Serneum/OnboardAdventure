1ad63d5d3ef3338b3837073d8bf569727ca4608e
Chris Rees <crees@covermymeds.com>
2019-05-31 14:21:28 -0400

    Add animations

--- player/player.gd
+++ player/player.gd
@@ -1,15 +1,30 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 const SPEED = 70
 
-var movedir = Vector2(0,0)
+var movedir = Vector2.ZERO
+var spritedir = "down"
+var validMoves = [
+	{dir = "up", vec = Vector2(0, -1)},
+	{dir = "down", vec = Vector2(0, 1)},
+	{dir = "left", vec = Vector2(-1, 0)},
+	{dir = "right", vec = Vector2(1, 0)}
+]
 
 func _physics_process(delta):
 	controls_loop()
 	movement_loop()
+	spritedir_loop()
+	
+	if movedir != Vector2.ZERO:
+		if is_on_wall():
+			for i in validMoves:
+				if spritedir == i.dir and test_move(transform, i.vec):
+					anim_switch("push")
+		else:
+			anim_switch("walk")
+	else:
+		anim_switch("idle")
 
 func controls_loop():
 	var LEFT	= Input.is_action_pressed("ui_left")
@@ -22,4 +37,20 @@ func controls_loop():
 
 func movement_loop():
 	var motion = movedir.normalized() * SPEED
-	move_and_slide(motion, Vector2(0, 0))
+	move_and_slide(motion, Vector2.ZERO)
+
+func spritedir_loop():
+	match movedir:
+		Vector2(-1, 0):
+			spritedir = "left"
+		Vector2(1, 0):
+			spritedir = "right"
+		Vector2(0, -1):
+			spritedir = "up"
+		Vector2(0, 1):
+			spritedir = "down"
+
+func anim_switch(animation):
+	var newanim = str(animation, spritedir)
+	if $anim.current_animation != newanim:
+		$anim.play(newanim)

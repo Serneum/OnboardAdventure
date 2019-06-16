4506606b2076eef2db1ad14a5872f62ab5706590
Chris Rees <crees@covermymeds.com>
2019-06-16 13:36:47 -0400

    Add sword swing sounds. Refactor

--- engine/item.gd
+++ engine/item.gd
@@ -4,19 +4,3 @@ var TYPE = null
 
 export(float) var DAMAGE = 1
 export(int) var maxamount = 1
-export(String) var animname = ""
-
-func _ready():
-	TYPE = get_parent().TYPE
-	$anim.connect("animation_finished", self, "destroy")
-	$anim.play(str(animname, get_parent().spritedir))
-	var methodname = str("state_", animname)
-	if get_parent().has_method(methodname):
-		get_parent().state = animname
-
-func destroy(animation):
-	queue_free()
-	
-	var methodname = str("state_", animname)
-	if get_parent().has_method(methodname):
-		get_parent().state = "default"
--- items/sword.gd
+++ items/sword.gd
@@ -1,1 +1,17 @@
 extends "res://engine/item.gd"
+
+func _ready():
+	TYPE = get_parent().TYPE
+	$anim.connect("animation_finished", self, "destroy")
+	$anim.play(str("swing", get_parent().spritedir))
+	
+	sfx.play(load(str("res://items/sword_swing",int(rand_range(1,5)),".wav")))
+	
+	if get_parent().has_method("state_swing"):
+		get_parent().state = "state_swing"
+
+func destroy(animation):
+	queue_free()
+	
+	if get_parent().has_method("state_swing"):
+		get_parent().state = "default"
